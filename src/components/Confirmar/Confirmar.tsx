e7fd5d11f8c85c81c1a3ddbdd843e3d88e4ca166
felipeerira9020 <felipe.erira9020@gmail.com>
2023-02-20 14:43:10 -0500

    asistencia

--- src/components/Confirmar/Confirmar.tsx
+++ src/components/Confirmar/Confirmar.tsx
@@ -5,7 +5,7 @@ const Confirmar = () => {
     return (
         <div>
             <h1>Confirmar asistencia</h1>
-            <a href="https://api.whatsapp.com/send?phone=573017548257&text=Hola!%20Quiero%20confirmar%20mi%20asistencia%20a%20tu%20boda%20:)" target="_blank">Confirmar asistencia</a>
+            <a href="https://api.whatsapp.com/send?phone=573017548257&text=Hola!%20Quiero%20confirmar%20mi%20asistencia%20al%20cumple%20de%20%Sofia%20%F0%9F%98%8A" target="_blank">Confirmar asistencia</a>
 
             
         </div>
