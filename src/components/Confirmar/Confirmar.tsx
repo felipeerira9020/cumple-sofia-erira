2fcff03113d3b5f3c9f2319572582813e525d9c0
felipeerira9020 <felipe.erira9020@gmail.com>
2023-02-20 15:00:05 -0500

    wspr

--- src/components/Confirmar/Confirmar.tsx
+++ src/components/Confirmar/Confirmar.tsx
@@ -8,7 +8,7 @@ const Confirmar = () => {
       >
             <H2>Confirmar asistencia</H2>
             <AiFillCaretDown className="flecha" />
-            <Redirigir href="https://api.whatsapp.com/send?phone=573017548257&text=Hola!%20Quiero%20confirmar%20mi%20asistencia%20al%20cumple%20de%20%Sofia%20%F0%9F%98%8A" target="_blank">
+            <Redirigir href="https://api.whatsapp.com/send?phone=573017548257&text=Hola!%20Quiero%20confirmar%20mi%20asistencia%20al%20cumple%20de%20Sofia🎂%20%F0%9F%98%8A" target="_blank">
                  <img src="https://cuidadovital.co/wp-content/uploads/2020/02/boton-whatsapp.png" alt="what"
                   style={{ width: "70%", height: "auto",marginRight : "auto", marginLeft : "auto" }}
                  />
