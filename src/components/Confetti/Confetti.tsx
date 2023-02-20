df1bdbb7841e7ef9f50346af5595cd1776f04789
felipeerira9020 <felipe.erira9020@gmail.com>
2023-02-20 15:39:55 -0500

    confe

--- src/components/Confetti/Confetti.tsx
+++ src/components/Confetti/Confetti.tsx
@@ -1,57 +1,43 @@
-import {useEffect, useState} from 'react'
-import ReactConfetti from 'react-confetti'
+import { useEffect, useState } from "react";
+import ReactConfetti from "react-confetti";
 
 const Confetti = () => {
-    const [windowWidth, setWindowWidth] = useState({width: window.innerWidth, height: window.innerHeight})
-    const [confeti, setConfeti] = useState(true)
+  const [windowWidth, setWindowWidth] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [confeti, setConfeti] = useState(true);
 
+  const detectSeze = () => {
+    setWindowWidth({ width: window.innerWidth, height: window.innerHeight });
+  };
 
-const detectSeze = () => {
-    setWindowWidth({width: window.innerWidth, height: window.innerHeight})
-}
+  useEffect(() => {
+    window.addEventListener("resize", detectSeze);
+    return () => {
+      window.removeEventListener("resize", detectSeze);
+    };
+  }, [windowWidth]);
 
-    useEffect(() => {
-        window.addEventListener('resize', detectSeze)
-        return () => {
-            window.removeEventListener('resize', detectSeze)
-        }
-    }, [windowWidth])
-
-
-    //que setConefi se pase a false a los 3 segundos
-    useEffect(() => {
-        setTimeout(() => {
-            setConfeti(false)
-        }, 7000)
-    }, [])
-
-
+  useEffect(() => {
+    setTimeout(() => {
+      setConfeti(false);
+    }, 9000);
+  }, []);
 
   return (
     <div>
-        {/* <ReactConfetti
-            width={windowWidth.width}
-            height={windowWidth.height}
-            tweenDuration={3000}
-        /> */}
-
-       {
-           confeti ? 
-           <ReactConfetti
-            width={windowWidth.width}
-            height={windowWidth.height}
-            tweenDuration={3000}
-        />  : <></>
-
-
-           }
-   
-
-      
+      {confeti ? (
+        <ReactConfetti
+          width={windowWidth.width}
+          height={windowWidth.height}
+          tweenDuration={3000}
+        />
+      ) : (
+        <></>
+      )}
     </div>
-  )
-}
-
+  );
+};
 
 export default Confetti;
-
