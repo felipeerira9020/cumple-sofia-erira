771635b869b6bb2ff08ae5e6f45ce1106d531d2d
felipeerira9020 <felipe.erira9020@gmail.com>
2023-02-20 15:37:47 -0500

    confetti

--- src/components/Confetti/Confetti.tsx
+++ src/components/Confetti/Confetti.tsx
@@ -3,6 +3,8 @@ import ReactConfetti from 'react-confetti'
 
 const Confetti = () => {
     const [windowWidth, setWindowWidth] = useState({width: window.innerWidth, height: window.innerHeight})
+    const [confeti, setConfeti] = useState(true)
+
 
 const detectSeze = () => {
     setWindowWidth({width: window.innerWidth, height: window.innerHeight})
@@ -16,13 +18,35 @@ const detectSeze = () => {
     }, [windowWidth])
 
 
+    //que setConefi se pase a false a los 3 segundos
+    useEffect(() => {
+        setTimeout(() => {
+            setConfeti(false)
+        }, 7000)
+    }, [])
+
+
+
   return (
     <div>
-        <ReactConfetti
+        {/* <ReactConfetti
             width={windowWidth.width}
             height={windowWidth.height}
             tweenDuration={3000}
-        />
+        /> */}
+
+       {
+           confeti ? 
+           <ReactConfetti
+            width={windowWidth.width}
+            height={windowWidth.height}
+            tweenDuration={3000}
+        />  : <></>
+
+
+           }
+   
+
       
     </div>
   )
