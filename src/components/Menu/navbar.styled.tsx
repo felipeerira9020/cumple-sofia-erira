e08fbfc0e76e80e9e21945d1d5a220558f4d56b3
felipeerira9020 <felipe.erira9020@gmail.com>
2023-02-21 19:30:08 -0500

    push

--- src/components/Menu/navbar.styled.tsx
+++ src/components/Menu/navbar.styled.tsx
@@ -10,73 +10,82 @@ export const MenuContainer = styled.nav`
     box-shadow: 0 0 10px 0 #0000006a;
     padding: 0px;
     font-weight: 100;
-
   }
 `;
 export const Button = styled.button`
-background: red;
-    cursor: pointer;
-    width: auto;
-    :hover{
-        background: blue;
-    }
-`
+  background: red;
+  cursor: pointer;
+  width: auto;
+  :hover {
+    background: blue;
+  }
+`;
 
-export const Nav = styled.div `
-
-            width: 70%;
-            display: block;
-            position: absolute;
-            top: 0;
-            left: auto;
-            right: 0;
-            text-align: center;
-            background: #27272c;
-            height: 100%;
-            z-index: 100;
-            opacity: 0.9;
-            
+export const Nav = styled.div`
+  width: 80%;
+  display: block;
+  position: absolute;
+  top: 0;
+  left: auto;
+  right: 0;
+  text-align: center;
+  background: #232324;
+  //que el background con degradado blanco y negro en forma de triangulo{}
+    background: linear-gradient(
+    50deg,
+    #dadada 0%,
+    #8f8d8d 10%,
+    #0a0a0a 50%,
+    #111111 100%
+    );
+    
         
-    `
-export const Close = styled.button `
-background: red;
+
+
+  height: 100%;
+  z-index: 100;
+  height: 100%;
+`;
+export const Close = styled.button`
+  background: red;
+  color: white;
+  font-size: 30px;
+  font-weight: 100;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  position: absolute;
+`;
+
+export const Menu = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    li {
+      margin: 20px 0;
+      a {
+        text-decoration: none;
         color: white;
-        font-size: 30px;
+        font-size: 25px;
         font-weight: 100;
-        border-radius: 50%;
-        width: 50px;
-        height: 50px;
-        position: absolute;
+        letter-spacing: 2px;
+        //que tenga decoracion arriba y abajo
 
-  
-
-        `
-
-   export const Menu = styled.div `
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 100%;
-        ul{
-            list-style: none;
-            padding: 0;
-            margin: 0;
-            li{
-                margin: 20px 0;
-                a{
-                    text-decoration: none;
-                    color: white;
-                    font-size: 20px;
-                    font-weight: 100;
-                }
-            }
-        }
-      img{
-        width: 200px;
-        height: auto; 
-        padding-top: 200px;
-      }
-   
-   
-   `     
+        text-shadow: 0 0 10px #0000006a;
+        text-decoration: underline;
+      
+    }
+    }
+  }
+  img {
+    width: 200px;
+    height: auto;
+    padding-top: 200px;
+  }
+`;
